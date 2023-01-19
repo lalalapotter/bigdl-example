e8b39cd0473bccff4faaedec64e59f00b5ca4ce3
lalalapotter <potterguang101@gmail.com>
2023-01-19 14:52:12 +0800

    feat: add prediction and data generation.

--- mastercard/lstm/README.md
+++
@@ -1,4 +0,0 @@
-pip install bigdl-orca==2.1.0
-pip install pandas
-pip install tensorflow
-pip install ray
--- mastercard/lstm/lstm.py
+++ mastercard/lstm/lstm.py
@@ -1,17 +1,44 @@
+import random
+
 from pyspark.ml.feature import VectorAssembler
-from bigdl.orca import init_orca_context, OrcaContext
-from bigdl.orca.learn.tf2 import Estimator
+from pyspark.sql.types import StructType, StructField, FloatType
 import tensorflow as tf
 
-sc = init_orca_context(cluster_mode="local", init_ray_on_spark=True)
-spark = OrcaContext.get_spark_session()
-data_filepath = "./data.csv"
+from bigdl.orca import init_orca_context, stop_orca_context, OrcaContext
+from bigdl.orca.learn.tf2 import Estimator
+
+
+sc = init_orca_context(cluster_mode="spark-submit", init_ray_on_spark=True)
+
+# generate random data
+records=10000
 dim = 128
-df = spark.read.option("inferSchema", "true").option('header', 'true').csv(data_filepath)
+print("Generating %d records\n" % records)
+spark = OrcaContext.get_spark_session()
+
+fieldnames = []
+for i in range(dim):
+  fieldnames.append('f-' + str(i))
+fieldnames.append('output')
+
+def map_func(x):
+  row = []
+  for f in fieldnames:
+      row.append(random.random())
+  return row
+
+fields = []
+for f in fieldnames:
+  fields.append(StructField(f, FloatType(), False))
+
+rdd = sc.range(0, records).map(map_func)
+schema = StructType(fields)
+df = spark.createDataFrame(rdd, schema)
+
 assembler = VectorAssembler(inputCols=df.columns[:dim], outputCol="input")
 df = assembler.transform(df)
-df.printSchema()
-df.show(5)
+train_df, test_df = df.randomSplit([0.8, 0.2], 24)
+
 # design network
 def model_creator(config):
     model = tf.keras.models.Sequential()
@@ -21,11 +48,12 @@ def model_creator(config):
     model.compile(loss='mae', optimizer='adam')
     return model
 
-estimator = Estimator.from_keras(model_creator=model_creator, backend="spark", worker_per_node=2)
+estimator = Estimator.from_keras(model_creator=model_creator, backend="ray", worker_per_node=2)
 epochs = 1
 batch_size = 32
+
 # fit network
-estimator.fit(data=df,
+estimator.fit(data=train_df,
               epochs=epochs,
               batch_size=batch_size,
               feature_cols=["input"],
@@ -33,5 +61,7 @@ estimator.fit(data=df,
               steps_per_epoch= df.count() // batch_size)
 
 # make a prediction
+estimator.predict(data=test_df,
+                  feature_cols=["input"])
 
-estimator.shutdown()
+stop_orca_context()
